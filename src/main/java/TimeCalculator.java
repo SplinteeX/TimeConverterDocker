996e8b0e24be8fc2283dbaaee027d559f16a7289
SplinteeX <Jepsteri04@gmail.com>
2024-09-27 13:36:16 +0300

    ..

--- src/main/java/TimeCalculator.java
+++ src/main/java/TimeCalculator.java
@@ -7,6 +7,15 @@ public class TimeCalculator {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello, Docker");
+        TimeCalculator calculator = new TimeCalculator();
+        double distance = 100.0;
+        double speed = 10.0;
+
+        try {
+            double time = calculator.calculateTime(distance, speed);
+            System.out.println("Time to travel " + distance + " km at " + speed + " km/h is: " + time + " hours.");
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
